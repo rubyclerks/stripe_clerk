31483c2b143992c5f3c3a4c4c6a1ae88c01dd3f0
Torsten Ruger <torsten@villataika.fi>
2015-12-23 13:28:49 +0200

    remove deprication

--- dummy/spec/support/capybara.rb
+++ dummy/spec/support/capybara.rb
@@ -4,7 +4,7 @@ Capybara::Screenshot.prune_strategy = :keep_last_run
 
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
-Capybara.default_wait_time = 10
+Capybara.default_max_wait_time = 10
 Capybara.default_selector = :css
 
 RSpec.configure do |config|
--- lib/stripe_clerk/engine.rb
+++ lib/stripe_clerk/engine.rb
@@ -1,8 +1,11 @@
+require "stripe"
+
 module StripeClerk
   class Engine < ::Rails::Engine
     isolate_namespace StripeClerk
 
     config.assets.precompile += %w( your_stripe_logo.png )
 
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
