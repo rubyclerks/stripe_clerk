8158b829a51ec0ea4dd1a93b1e63415c25478601
Torsten Ruger <torsten@villataika.fi>
2015-12-23 13:33:24 +0200

    split specs into two fail and succeed

---
+++ dummy/spec/features/fail_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'office_clerk'
+
+describe "pay with stripe", :js => true do
+  before do
+    order = create :order
+    page.set_rack_session order: order.id
+    page.visit shop_order_path
+  end
+  it "has errors" do
+    click_button I18n.t("stripe_clerk.pay_with_card")
+    stripe_iframe = all('iframe[name=stripe_checkout_app]').last
+    Capybara.within_frame stripe_iframe do
+        fill_in "card_number", :with => '4000000000000002'
+      fill_in "cc-exp", :with => '11/20'
+      fill_in "cc-csc", :with => '123'
+      screenshot_and_open_image
+      click_button I18n.t("stripe_clerk.pay_with_card")
+      expect(page).to have_content("This card was declined")
+    end
+  end
+end
