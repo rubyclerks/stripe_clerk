83e77f87fbde3a32341ce95d80ef6d7c14ede2b4
Torsten Ruger <torsten@villataika.fi>
2015-12-23 10:48:14 +0200

    save charge again
    my misunderstanding of stripe
    helper gets last charge
    with charge.customer one can make next charge

--- app/helpers/stripe_clerk/charges_helper.rb
+++ app/helpers/stripe_clerk/charges_helper.rb
@@ -1,21 +1,25 @@
 module StripeClerk
   module ChargesHelper
 
-    # determine if the given user has ordered by card and if so return that order
-    # given user may be nil, in which case nil is returned (save caler checking)
+    # determine if the given user has ordered by card and if so return the charge
+    # given user may be nil, in which case nil is returned (save caller checking)
     # nil is also returned if no stripe order was placed
-    def used_card user
+    # from the returned charge one may get the card (to display last4) or
+    #    the customer to create a new charge (with the charge_customer method)
+    def has_charge user
       return nil unless user
-      Order.where(email: user.email).where(payment_type: :stripe).order(:ordered_on).last
+      order = Order.where(email: user.email).where(payment_type: :stripe).order(:ordered_on).last
+      return nil unless order
+      Stripe::Charge.retrieve(order.payment_info)
     end
 
     # reusable helper to charge given customer_id for given order
-    # orders save the customer_id, so customers may be re-charged using that
-    #
+    # orders save the charge_id, so customers may be re-charged using that
+    # (has_charge helper will return the latest charge for a user and .customer will give it's id)
     # Throws any StripeError , specifically CardError
     #
     def charge_customer customer_id , order
-      Stripe::Charge.create(
+      charge = Stripe::Charge.create(
         :customer    => customer_id,
         :amount      => (order.total_price*100).to_i,
         :description => t(:order) + " : " + order.number ,
@@ -23,7 +27,7 @@ module StripeClerk
       )
 
       order.pay_now
-      order.payment_info = customer_id
+      order.payment_info = charge.id
       order.payment_type = "stripe"
       order.save
     end
